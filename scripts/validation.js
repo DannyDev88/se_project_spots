c74665b43fde58fa0bdf8f93977c0c1a2b5d3ba5
Daniel Martinez <shmackboy@Shmacks-Mac.local>
2025-03-26 16:20:15 -0400

    Project 6 Unfinished 3

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,6 +1,33 @@
+const showInputError(formEl, inputEl, errorMsg) => {
+
+}
+
+const checkInputValidity = (formEl, inputEl) => {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, inputEl.validationMessage);
+  }
+};
+
+const setEventListeners = (formEl) => {
+  const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
+  const buttonElement = formEl.querySelector(".modal__submit-btn");
+
+  // TODO - handle initial states
+  // toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputEl) => {
+    inputEl.addEventListener("input", function () {
+      checkInputValidity(formEl, inputEl);
+      // toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
 const enableValidation = () => {
   const formList = Array.from(document.querySelectorAll(".modal__form"));
-  console.log(formList);
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
 };
 
 enableValidation();
